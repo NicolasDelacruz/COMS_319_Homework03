21639995b526f692e89e7701e2216d4379d28212
Nicolas De la Cruz <nicolasd@iastate.edu>
2019-10-14 12:19:01 -0500

    first commit

---
+++ snake.js
@@ -0,0 +1,15 @@
+const fillHeight = 5;
+const fillWidth = 5;
+
+let direction = 'west';
+
+let i = 100;
+let timer = setInterval(function () {
+    document.getElementById("canvasId").innerHTML = i--;
+}, 1000);
+
+function leftTurn(){
+}
+
+function rightTurn() {
+}
